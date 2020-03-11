63e53a97aad8412650a7a795ff92d84c4e8602ab
Vlad <Vlad@DESKTOP-HJ9OVGE>
2020-03-11 20:56:32 +0200

    Initilize

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            double a = Counter(1, 2);
+            double b = await CounterAsync(1, 2);
+        }
+        static Task<double> CounterAsync(int a, int b)
+        {
+            return Task.Run(() => Counter(a, b));
+        }
+
+        static double Counter(int a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
---
+++ TaskConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    class Program
+    {
+        static CancellationTokenSource tokenSource;
+        static CancellationToken cancellationToken;
+
+        static void Main(string[] args)
+        {
+            tokenSource = new CancellationTokenSource();
+            cancellationToken = tokenSource.Token;
+
+            Action action = new Action(EasyWork);
+
+            Task ts = new Task(action,cancellationToken);
+            ts.Start();
+            ts.Wait();
+
+            var taskResult = Task.Run(action);
+            Thread.Sleep(500);
+            tokenSource.Cancel();
+
+            var taskResult2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello world from Run Task");
+                }
+            });
+
+            taskResult2.Wait();
+
+            var taskResult3 = Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello world from Run Task");
+                }
+                return true;
+            });
+
+            taskResult3.Wait();
+            Console.WriteLine(taskResult3.Result);
+
+            var taskresult4 = Task.Run(() => Counter(4, 5));
+            taskresult4.Wait();
+            Console.WriteLine(taskresult4.Result);
+        }
+
+        static void EasyWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if(cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                Console.WriteLine($"Easy {i} Hello World!!!");
+                Thread.Sleep(100);
+                Console.WriteLine($"Current Th={Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+
+        static double Counter(double a, double b) => a + b;
+    }
+}
